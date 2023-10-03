0d55f10312c33d13c829b05647ee058010f56f02
Victoria <victoria.reguyal@gmail.com>
2023-10-02 22:24:53 -0400

    launch er

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Launcher.java
@@ -1,4 +1,22 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 public class Launcher {
+
+    // No clue what I'm doing ngl
+
+    public static double LAUNCH = 0;
+    public static double IDLE = 0;
+
+    public void init(HardwareMap hwMap) {
+        launcher = hwMap.get(Servo.class, "launcher");
+    }
+
+    public void launch() {
+        launcher.setPosition(LAUNCH);
+    }
+
+    public void reset() {
+        launcher.setPosition(IDLE);
+    }
+
 }
