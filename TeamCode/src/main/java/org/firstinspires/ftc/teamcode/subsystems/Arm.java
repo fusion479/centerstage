80761872f9e9631524692dae1821d74b7cc0b36c
Princeden <princedenhom@gmail.com>
2023-10-03 19:37:15 -0400

    basic arm code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
@@ -1,5 +1,34 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-public class Arm {
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
+public class Arm {
+    Servo left, right;
+    private double setPos = 0.44;
+    private boolean up;
+    public void init(HardwareMap hwmap) {
+        left = hwmap.get(Servo.class, "armLeft");
+        right = hwmap.get(Servo.class, "armRight");
+        up = false;
+    }
+    public void setUp() {
+        left.setPosition(setPos);
+        right.setPosition(setPos);
+        up = true;
+    }
+    public void setDown() {
+        left.setPosition(0);
+        right.setPosition(0);
+        up = false;
+    }
+    public void toggle() {
+        if (up) {
+            setDown();
+        }
+        else {
+            setUp();
+        }
+
+    }
 }
