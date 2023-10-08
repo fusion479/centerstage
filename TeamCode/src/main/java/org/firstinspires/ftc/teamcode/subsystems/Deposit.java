c4c050b51bd568e5b2821910dc553c19cceaf74a
ItJustSamX <129796050+ItJustSamX@users.noreply.github.com>
2023-10-08 00:48:48 -0400

    deposit code thats probably fucked cuz im imcompetent

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Deposit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Deposit.java
@@ -1,4 +1,38 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class Deposit {
+  public static double intakePos = 0.69;
+  public static double scorePos = 0.69;
+
+  public static double open = 0.69;
+  public static double close = 0.69;
+  public void init(HardwareMap hwMap) {
+        claw = hwMap.get(Servo.class, "clawDep");
+        left = hwMap.get(Servo.class, "Left");
+        right = hwMap.get(Servo.class, "Right");
+  }
+  
 }
+
+public void collect {
+  left.setPosition(intakePos);
+  right.setPosition(intakePos - 1);
+}
+
+public void score {
+  left.setPosition(scorePos);
+  right.setPosition(scorePos - 1);
+}
+
+public void open {
+  claw.setPosition(open)
+}
+
+public void close {
+  claw.setPosition(close)
+}
+
+// i hope i know what im doing
