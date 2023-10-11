55db695d0fe18493a5cb48dd8ede44bdc0be3b9a
khin101-stuy <kaung40@stuy.edu>
2023-10-10 20:20:03 -0400

    never let me cook

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -1,4 +1,31 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import android.widget.ToggleButton;
+
 public class Intake {
+    public boolean Toggle = false;
+    public static float intakeVoltage = ;//idfk
+    public static float idleVoltage = 0;
+
+    public void init(HardwareMap hwMap) {
+        intakeMotor = hwMap.get(Motor.class, "intakeMotor");
+    }
 }
+
+    public static void intakeToggle()
+{
+        while // controller logic idk
+        {
+        Toggle = true;
+        }
+        else Toggle = flase;
+        }
+public static void intakeMotorVoltage()
+        {
+            if (Toggle) {
+            }
+            intakeMotor.voltage = intakeVoltage;
+            }
+            else
+
+        }
