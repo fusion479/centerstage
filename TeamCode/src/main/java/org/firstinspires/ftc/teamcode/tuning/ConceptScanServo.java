bcef1ac1c28710f3e5c2dbefdcd2502400ba3029
Princeden <princedenhom@gmail.com>
2023-11-10 16:47:44 -0500

    fixed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/ConceptScanServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/ConceptScanServo.java
@@ -32,6 +32,7 @@ package org.firstinspires.ftc.teamcode.tuning;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.Servo;
 
 /*
@@ -51,61 +52,27 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "Concept: Scan Servo", group = "Concept")
 @Disabled
 public class ConceptScanServo extends LinearOpMode {
+    AnalogInput axonBoard;
 
-    static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
-    static final int    CYCLE_MS    =   50;     // period of each cycle
-    static final double MAX_POS     =  1.0;     // Maximum rotational position
-    static final double MIN_POS     =  0.0;     // Minimum rotational position
-
-    // Define class members
-    Servo   servo;
-    double  position = (MAX_POS - MIN_POS) / 2; // Start at halfway position
-    boolean rampUp = true;
+    double reading = 0;
 
 
     @Override
     public void runOpMode() {
 
-        // Connect to servo (Assume Robot Left Hand)
-        // Change the text in quotes to match any servo name on your robot.
-        servo = hardwareMap.get(Servo.class, "left_hand");
-
+        axonBoard =  hardwareMap.get(AnalogInput.class, "axonBoard");
         // Wait for the start button
         telemetry.addData(">", "Press Start to scan Servo." );
         telemetry.update();
+
         waitForStart();
 
 
         // Scan servo till stop pressed.
         while(opModeIsActive()){
-
-            // slew the servo, according to the rampUp (direction) variable.
-            if (rampUp) {
-                // Keep stepping up until we hit the max value.
-                position += INCREMENT ;
-                if (position >= MAX_POS ) {
-                    position = MAX_POS;
-                    rampUp = !rampUp;   // Switch ramp direction
-                }
-            }
-            else {
-                // Keep stepping down until we hit the min value.
-                position -= INCREMENT ;
-                if (position <= MIN_POS ) {
-                    position = MIN_POS;
-                    rampUp = !rampUp;  // Switch ramp direction
-                }
-            }
-
-            // Display the current value
-            telemetry.addData("Servo Position", "%5.2f", position);
-            telemetry.addData(">", "Press Stop to end test." );
+            reading = axonBoard.getVoltage() / 3.3 * 360;
+            telemetry.addData("Encoder Reading", "%5.2f", reading);
             telemetry.update();
-
-            // Set the servo to the new position and pause;
-            servo.setPosition(position);
-            sleep(CYCLE_MS);
-            idle();
         }
 
         // Signal done;
