36c7f1021e6deedd9e0d9e440019fb973a157395
Bi1ku <2008owenshi@gmail.com>
2023-12-10 22:23:13 -0500

    command

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/ScoreCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/ScoreCommand.java
@@ -5,3 +5,8 @@ import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 public class ScoreCommand extends SequentialCommandGroup {
     
 }
+
+/*
+    github for version control new branch:
+    https://github.com/fusion479/centerstage/tree/command
+*/
