e6bd33f1ce6ea2d972dd77181c2c0399f50d8d5a
Princeden <princedenhom@gmail.com>
2024-01-08 16:01:24 -0500

    intake

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/main/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/main/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting.main;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -13,15 +14,10 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(15.5, 63.25, Math.toRadians(-90)))
+                                .setReversed(true) //spike 1
+                                .splineToLinearHeading(new Pose2d(8, 37, Math.toRadians(-90)), Math.toRadians(135))
+
                                 .build()
                 );
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/Intake.java
@@ -4,17 +4,22 @@ import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @Config
 public class Intake extends Mechanism {
 
     DcMotorEx intake;
+    Servo intakeLeft;
+    Servo intakeRight;
+    public static double UP_POS;
+    public static double DOWN_POS;
+    private boolean isUp;
     double power;
 
     @Override
     public void init(HardwareMap hwMap) {
         intake = hwMap.get(DcMotorEx.class, "intake");
-
         intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         intake.setDirection(DcMotorEx.Direction.FORWARD);
@@ -27,5 +32,29 @@ public class Intake extends Mechanism {
     public void setPower(double power) {
         this.power = power;
     }
+
+    public void raise() {
+        intakeLeft.setPosition(UP_POS);
+        intakeRight.setPosition(1-UP_POS);
+        isUp = true;
+    }
+    public void lower() {
+        intakeLeft.setPosition(DOWN_POS);
+        intakeRight.setPosition(1-DOWN_POS);
+        isUp = false;
+    }
+    public void toggle() {
+        if (isUp) {
+            raise();
+        }
+        else {
+            lower();
+        }
+    }
+
+
+
+
+
 
 }
