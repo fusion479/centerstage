8a7041b3b2de350a546568f209453495709a5f33
pyshrekek <rukereero@gmail.com>
2024-03-12 08:03:51 -0400

    quite a small change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/Arm.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @Config
 public class Arm extends Mechanism {
-    public static double UP_POS = 0.14;
+    public static double UP_POS = 0.1;
     public static double READY_POS = 0.7;
     public static double DOWN_POS = 0.72;
     public static double CLIMB_POS = 0.26;
