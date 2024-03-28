d89739c5c36010a3478a935efe21098967c8327f
Bi1ku <2008owenshi@gmail.com>
2024-03-28 15:23:57 -0400

    inner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/Deposit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/Deposit.java
@@ -14,8 +14,8 @@ public class Deposit extends Mechanism {
     public static double AUTO_INIT_POS = 0;
     public static double STACK_TOP_POS = 0.52;
 
-    public static double LOCK_INNER = 0.4;
-    public static double LOCK_OUTER = 0.25;
+    public static double LOCK_INNER = 0.5;
+    public static double LOCK_OUTER = 0.7;
 
     public static double OPEN_INNER = 0.3;
     public static double OPEN_OUTER = 0.35;
