151c10df1e21d04c4ecf4c5918a73f849f08693b
Bi1ku <2008owenshi@gmail.com>
2024-04-27 11:25:20 -0400

    init constants and trajectory sturctures

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/AutonTrajectories.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode.opmodes.auton;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.camera.Camera;
+
+public class AutonTrajectories {
+    private final Camera.Color color;
+    private final MecanumDrive drive;
+
+    public AutonTrajectories(Camera.Color color, Drivetrain drivetrain) {
+        this.color = color;
+        this.drive = drivetrain.drive;
+    }
+
+    public Pose2d reflectY(Pose2d pose) {
+        return this.color == Camera.Color.RED ? new Pose2d(pose.position.x, -pose.position.y, Math.toRadians(360) - pose.heading.real) : pose;
+    }
+
+    public class Far {
+        // SPIKEMARK
+        public Action MID_SPIKEMARK = drive.actionBuilder(drive.pose)
+                .build();
+
+        public Action RIGHT_SPIKEMARK = drive.actionBuilder(drive.pose)
+                .build();
+
+        public Action LEFT_SPIKEMARK = drive.actionBuilder(drive.pose)
+                .build();
+
+        // BACKDROP
+        public Action MID_BACKDROP = drive.actionBuilder(drive.pose)
+                .build();
+
+        public Action RIGHT_BACKDROP = drive.actionBuilder(drive.pose)
+                .build();
+
+        public Action LEFT_BACKDROP = drive.actionBuilder(drive.pose)
+                .build();
+
+        // PARK
+        public Action PARK = drive.actionBuilder(drive.pose)
+                .build();
+    }
+
+    public class Close {
+        // SPIKEMARK
+        public Action MID_SPIKEMARK = drive.actionBuilder(drive.pose)
+                .build();
+
+        public Action RIGHT_SPIKEMARK = drive.actionBuilder(drive.pose)
+                .build();
+
+        public Action LEFT_SPIKEMARK = drive.actionBuilder(drive.pose)
+                .build();
+
+        // BACKDROP
+        public Action MID_BACKDROP = drive.actionBuilder(drive.pose)
+                .build();
+
+        public Action RIGHT_BACKDROP = drive.actionBuilder(drive.pose)
+                .build();
+
+        public Action LEFT_BACKDROP = drive.actionBuilder(drive.pose)
+                .build();
+
+        // PARK
+        public Action PARK = drive.actionBuilder(drive.pose)
+                .build();
+    }
+}
