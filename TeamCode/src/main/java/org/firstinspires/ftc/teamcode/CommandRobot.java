6034f480b82e84cd0d109925759594975109123b
Bi1ku <2008owenshi@gmail.com>
2024-05-18 12:40:46 -0400

    implement moduarlize cmds

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandRobot.java
@@ -1,37 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.arcrobotics.ftclib.command.InstantCommand;
-import com.arcrobotics.ftclib.command.ParallelCommandGroup;
 import com.arcrobotics.ftclib.command.Robot;
-import com.arcrobotics.ftclib.command.SequentialCommandGroup;
-import com.arcrobotics.ftclib.command.WaitCommand;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.commands.arm.ArmAccepting;
-import org.firstinspires.ftc.teamcode.commands.arm.ArmReady;
-import org.firstinspires.ftc.teamcode.commands.arm.ArmScore;
-import org.firstinspires.ftc.teamcode.commands.deposit.DepositAccepting;
-import org.firstinspires.ftc.teamcode.commands.deposit.DepositReady;
-import org.firstinspires.ftc.teamcode.commands.deposit.DepositScore;
+import org.firstinspires.ftc.teamcode.commands.Commands;
 import org.firstinspires.ftc.teamcode.commands.deposit.locks.LockInner;
 import org.firstinspires.ftc.teamcode.commands.deposit.locks.LockOuter;
-import org.firstinspires.ftc.teamcode.commands.deposit.locks.OpenInner;
-import org.firstinspires.ftc.teamcode.commands.deposit.locks.OpenOuter;
 import org.firstinspires.ftc.teamcode.commands.drivetrain.ManualDrive;
-import org.firstinspires.ftc.teamcode.commands.intake.IntakeAccepting;
-import org.firstinspires.ftc.teamcode.commands.intake.IntakeReady;
-import org.firstinspires.ftc.teamcode.commands.launcher.Idle;
-import org.firstinspires.ftc.teamcode.commands.launcher.Launch;
-import org.firstinspires.ftc.teamcode.commands.lift.BottomLift;
-import org.firstinspires.ftc.teamcode.commands.lift.HighLift;
-import org.firstinspires.ftc.teamcode.commands.lift.LiftLower;
-import org.firstinspires.ftc.teamcode.commands.lift.LiftRaise;
-import org.firstinspires.ftc.teamcode.commands.lift.LowLift;
-import org.firstinspires.ftc.teamcode.commands.lift.MediumLift;
 import org.firstinspires.ftc.teamcode.subsystems.Arm;
 import org.firstinspires.ftc.teamcode.subsystems.Deposit;
 import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
@@ -41,25 +20,20 @@ import org.firstinspires.ftc.teamcode.subsystems.Lift;
 import org.firstinspires.ftc.teamcode.utils.GamepadTrigger;
 
 public class CommandRobot extends Robot {
-    private final Arm arm;
     private final Drivetrain drive;
-    private final Lift lift;
-    private final Launcher launcher;
-    private final Intake intake;
     private final Deposit deposit;
+    private final Intake intake;
     private final GamepadEx gamepad1;
     private final GamepadEx gamepad2;
     private final GamepadTrigger intakeAccept;
     private final GamepadTrigger intakeReject;
     private final ElapsedTime timer = new ElapsedTime();
+    public Commands commands;
     private boolean autoLocked = false;
 
-    public CommandRobot(final HardwareMap hwMap, final GamepadEx gamepad1, final GamepadEx gamepad2, final MultipleTelemetry telemetry) { // Create different bots for teleop, testing, and auton?
+    public CommandRobot(final HardwareMap hwMap, final GamepadEx gamepad1, final GamepadEx gamepad2, final MultipleTelemetry telemetry) {
         this.deposit = new Deposit(hwMap, telemetry);
-        this.arm = new Arm(hwMap, telemetry);
         this.drive = new Drivetrain(hwMap, telemetry);
-        this.lift = new Lift(hwMap, telemetry);
-        this.launcher = new Launcher(hwMap, telemetry);
         this.intake = new Intake(hwMap, telemetry);
 
         this.gamepad1 = gamepad1;
@@ -67,6 +41,7 @@ public class CommandRobot extends Robot {
 
         this.intakeAccept = new GamepadTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER, this.intake::setPower, this.gamepad1);
         this.intakeReject = new GamepadTrigger(GamepadKeys.Trigger.LEFT_TRIGGER, d -> this.intake.setPower(-d), this.gamepad1);
+        this.commands = new Commands(new Arm(hwMap, telemetry), new Lift(hwMap, telemetry), new Launcher(hwMap, telemetry), new Intake(hwMap, telemetry), this.deposit, this.timer, () -> this.autoLocked = false, this.timer::reset);
 
         this.drive.setDefaultCommand(new ManualDrive(this.drive, this.gamepad1));
 
@@ -75,105 +50,40 @@ public class CommandRobot extends Robot {
 
     public void configureCommands() {
         this.gamepad1.getGamepadButton(GamepadKeys.Button.A)
-                .toggleWhenPressed(new SequentialCommandGroup(
-                        new LowLift(this.lift),
-                        new ArmAccepting(this.arm),
-                        new IntakeAccepting(this.intake),
-                        new OpenInner(this.deposit),
-                        new OpenOuter(this.deposit),
-                        new DepositAccepting(this.deposit),
-                        new BottomLift(this.lift)
-                ), new SequentialCommandGroup(
-                        new LockInner(this.deposit),
-                        new LockOuter(this.deposit),
-                        new LowLift(this.lift),
-                        new ArmReady(this.arm),
-                        new DepositReady(this.deposit),
-                        new IntakeReady(this.intake),
-                        new BottomLift(this.lift)
-                ));
-
+                .toggleWhenPressed(this.commands.intake, this.commands.ready);
 
         // LIFT LOW
         this.gamepad1.getGamepadButton(GamepadKeys.Button.B)
-                .whenPressed(new ParallelCommandGroup(
-                        new LockOuter(this.deposit),
-                        new LockInner(this.deposit),
-                        new LowLift(this.lift),
-                        new ArmScore(this.arm),
-                        new DepositScore(this.deposit),
-                        new IntakeReady(this.intake)
-                ));
+                .whenPressed(this.commands.scoreLow);
         // LIFT HIGH
         this.gamepad1.getGamepadButton(GamepadKeys.Button.Y)
-                .whenPressed(new ParallelCommandGroup(
-                        new LockOuter(this.deposit),
-                        new LockInner(this.deposit),
-                        new HighLift(this.lift),
-                        new ArmScore(this.arm),
-                        new DepositScore(this.deposit),
-                        new IntakeReady(this.intake)));
+                .whenPressed(this.commands.scoreHigh);
 
         // LIFT MEDIUM
         this.gamepad1.getGamepadButton(GamepadKeys.Button.X)
-                .whenPressed(new ParallelCommandGroup(
-                        new LockOuter(this.deposit),
-                        new LockInner(this.deposit),
-                        new MediumLift(this.lift),
-                        new ArmScore(this.arm),
-                        new DepositScore(this.deposit),
-                        new IntakeReady(this.intake)));
+                .whenPressed(this.commands.scoreMid);
 
         // LIFT UP A LITTLE
         this.gamepad1.getGamepadButton(GamepadKeys.Button.DPAD_UP)
-                .whenPressed(new SequentialCommandGroup(
-                        new LockOuter(this.deposit),
-                        new LockInner(this.deposit),
-                        new ArmScore(this.arm),
-                        new DepositScore(this.deposit),
-                        new IntakeReady(this.intake),
-                        new LiftRaise(this.lift)));
+                .whenPressed(this.commands.liftRaise);
 
         // LIFT DOWN A LITTLE
         this.gamepad1.getGamepadButton(GamepadKeys.Button.DPAD_DOWN)
-                .whenPressed(new SequentialCommandGroup(
-                        new LockOuter(this.deposit),
-                        new LockInner(this.deposit),
-                        new ArmScore(this.arm),
-                        new DepositScore(this.deposit),
-                        new IntakeReady(this.intake),
-                        new LiftLower(this.lift)));
+                .whenPressed(this.commands.liftLower);
 
         // SCORE PIXEL ONE
         this.gamepad1.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER)
-                .whenPressed(new SequentialCommandGroup(
-                        new OpenOuter(this.deposit),
-                        new WaitCommand(175),
-                        new LiftRaise(this.lift),
-                        new InstantCommand(() -> {
-                            this.autoLocked = false;
-                            this.timer.reset();
-                        })
-                ));
+                .whenPressed(this.commands.scoreOne);
 
         // SCORE PIXEL TWO
         this.gamepad1.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER)
-                .whenPressed(new SequentialCommandGroup(
-                        new OpenInner(this.deposit),
-                        new InstantCommand(() -> this.autoLocked = false),
-                        new WaitCommand(175),
-                        new LiftRaise(this.lift),
-                        new InstantCommand(() -> {
-                            this.autoLocked = false;
-                            this.timer.reset();
-                        }
-                        )));
+                .whenPressed(this.commands.scoreTwo);
 
         // LAUNCHER COMMANDS
         this.gamepad2.getGamepadButton(GamepadKeys.Button.X)
-                .whenPressed(new Launch(this.launcher));
+                .whenPressed(this.commands.launch);
         this.gamepad2.getGamepadButton(GamepadKeys.Button.A)
-                .whenPressed(new Idle(this.launcher));
+                .whenPressed(this.commands.launchIdle);
     }
 
     public void updateTriggers() {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/Commands.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/Commands.java
@@ -5,7 +5,6 @@ import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.ParallelCommandGroup;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.arcrobotics.ftclib.command.WaitCommand;
-import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.commands.arm.ArmReady;
@@ -17,22 +16,24 @@ import org.firstinspires.ftc.teamcode.commands.deposit.locks.LockOuter;
 import org.firstinspires.ftc.teamcode.commands.deposit.locks.OpenInner;
 import org.firstinspires.ftc.teamcode.commands.deposit.locks.OpenOuter;
 import org.firstinspires.ftc.teamcode.commands.intake.IntakeReady;
+import org.firstinspires.ftc.teamcode.commands.launcher.Idle;
+import org.firstinspires.ftc.teamcode.commands.launcher.Launch;
 import org.firstinspires.ftc.teamcode.commands.lift.BottomLift;
 import org.firstinspires.ftc.teamcode.commands.lift.HighLift;
+import org.firstinspires.ftc.teamcode.commands.lift.LiftLower;
 import org.firstinspires.ftc.teamcode.commands.lift.LiftRaise;
 import org.firstinspires.ftc.teamcode.commands.lift.LowLift;
 import org.firstinspires.ftc.teamcode.commands.lift.MediumLift;
 import org.firstinspires.ftc.teamcode.subsystems.Arm;
 import org.firstinspires.ftc.teamcode.subsystems.Deposit;
-import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.subsystems.Launcher;
 import org.firstinspires.ftc.teamcode.subsystems.Lift;
 
 public class Commands {
-    public Command ready, intake, scoreLow, scoreHigh, scoreMid, liftRaise, liftLower, scoreOne, scoreTwo, launch;
+    public Command ready, intake, scoreLow, scoreHigh, scoreMid, liftRaise, liftLower, scoreOne, scoreTwo, launch, launchIdle;
 
-    public Commands(Arm arm, Drivetrain drive, Lift lift, Launcher launcher, Intake intake, GamepadEx gamepad1, Deposit deposit, ElapsedTime timer, Runnable autoLockFalse, Runnable resetTimer) {
+    public Commands(Arm arm, Lift lift, Launcher launcher, Intake intake, Deposit deposit, ElapsedTime timer, Runnable autoLockFalse, Runnable resetTimer) {
         this.ready = new SequentialCommandGroup(new LockInner(deposit),
                 new LockOuter(deposit),
                 new LowLift(lift),
@@ -83,6 +84,14 @@ public class Commands {
                 new IntakeReady(intake),
                 new LiftRaise(lift));
 
+        this.liftLower = new SequentialCommandGroup(
+                new LockOuter(deposit),
+                new LockInner(deposit),
+                new ArmScore(arm),
+                new DepositScore(deposit),
+                new IntakeReady(intake),
+                new LiftLower(lift));
+
         this.scoreOne = new SequentialCommandGroup(
                 new OpenOuter(deposit),
                 new WaitCommand(175),
@@ -101,5 +110,8 @@ public class Commands {
                     autoLockFalse.run();
                     resetTimer.run();
                 }));
+
+        this.launch = new Launch(launcher);
+        this.launchIdle = new Idle(launcher);
     }
 }
