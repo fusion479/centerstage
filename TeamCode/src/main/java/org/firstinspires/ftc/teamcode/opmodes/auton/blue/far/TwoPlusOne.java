8f351302f921ba05e6e7e28ff39a1058b8ec07c9
Bi1ku <2008owenshi@gmail.com>
2024-06-20 09:13:28 -0400

    clean up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/blue/far/TwoPlusOne.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/blue/far/TwoPlusOne.java
@@ -24,8 +24,6 @@ import org.firstinspires.ftc.teamcode.utils.CommandAction;
 public class TwoPlusOne extends CommandOpMode {
     private MultipleTelemetry multipleTelemetry;
     private CommandRobot robot;
-    private Trajectories.Far FAR;
-    private Trajectories.General GENERAL;
     private Camera camera;
 
     @Override
@@ -34,8 +32,6 @@ public class TwoPlusOne extends CommandOpMode {
         this.robot = new CommandRobot(super.hardwareMap, new GamepadEx(this.gamepad1), new GamepadEx(this.gamepad2), this.multipleTelemetry, Positions.FAR.START);
         this.camera = new Camera(Camera.Color.BLUE, this.multipleTelemetry);
         this.camera.initCamera(super.hardwareMap);
-
-        this.FAR = new Trajectories(Camera.Color.BLUE, this.robot.getDrive()).new Far();
     }
 
     @Override
@@ -48,10 +44,11 @@ public class TwoPlusOne extends CommandOpMode {
             this.multipleTelemetry.update();
         }
         int region = this.camera.getRegion();
-
-        Action initialPath = region == 1 ? this.FAR.LEFT_SPIKEMARK : region == 2 ? this.FAR.MID_SPIKEMARK : this.FAR.RIGHT_SPIKEMARK;
-
         this.camera.stopStreaming();
+
+        Trajectories.Far FAR = new Trajectories(Camera.Color.RED, this.robot.getDrive()).new Far();
+        Action initialPath = region == 1 ? FAR.LEFT_SPIKEMARK : region == 2 ? FAR.MID_SPIKEMARK : FAR.RIGHT_SPIKEMARK;
+
         Actions.runBlocking(new ParallelAction(
                 initialPath,
                 new SequentialAction(
@@ -61,8 +58,8 @@ public class TwoPlusOne extends CommandOpMode {
                 )
         ));
 
-        this.GENERAL = new Trajectories(Camera.Color.BLUE, this.robot.getDrive()).new General();
-        Action stackToBackdrop = region == 1 ? this.GENERAL.STACK_TO_LEFT_BACKDROP : region == 2 ? this.GENERAL.STACK_TO_MID_BACKDROP : this.GENERAL.STACK_TO_RIGHT_BACKDROP;
+        Trajectories.General GENERAL = new Trajectories(Camera.Color.RED, this.robot.getDrive()).new General();
+        Action stackToBackdrop = region == 1 ? GENERAL.STACK_TO_LEFT_BACKDROP : region == 2 ? GENERAL.STACK_TO_MID_BACKDROP : GENERAL.STACK_TO_RIGHT_BACKDROP;
 
         Actions.runBlocking(new ParallelAction(
                 stackToBackdrop,
@@ -78,7 +75,7 @@ public class TwoPlusOne extends CommandOpMode {
                 )
         ));
 
-        Actions.runBlocking(this.FAR.getPark());
+        Actions.runBlocking(FAR.getPark());
 
         CommandScheduler.getInstance().cancelAll();
         CommandScheduler.getInstance().disable();
