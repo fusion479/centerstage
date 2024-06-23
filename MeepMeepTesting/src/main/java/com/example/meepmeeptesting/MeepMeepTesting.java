d61468a09150c3e8ffb9cc54e51c9214823453dd
Bi1ku <2008owenshi@gmail.com>
2024-06-22 21:16:11 -0400

    working

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -6,19 +6,23 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class MeepMeepTesting {
-    private enum Color {
-        BLUE, RED
-    }
-    private final Color color = Color.RED; // Change this for different colors
-    private Pose2d reflectY(Pose2d pose) {
-        return this.color == Color.RED ? new Pose2d(pose.position.x, -pose.position.y, Math.toRadians(360) - pose.heading.imag) : pose;
+class TestingConstants {
+    final int color = 1; // 0 = blue, 1 = red
+    final double Y_OFFSET = -3.0;
+    final double X_OFFSET = 3.0;
+
+    Pose2d reflectY(Pose2d pose) {
+        return color == 1 ? new Pose2d(pose.position.x, -pose.position.y, Math.toRadians(360) - pose.heading.imag) : pose;
     }
 
-    private Vector2d reflectY(Vector2d vector) {
-        return this.color == Color.RED ? new Vector2d(vector.x, -vector.y) : vector;
+    Vector2d reflectY(Vector2d vector) {
+        return color == 1 ? new Vector2d(vector.x, -vector.y) : vector;
     }
+}
+public class MeepMeepTesting {
     public static void main(String[] args) {
+        TestingConstants testingConstants = new TestingConstants();
+
         MeepMeep meepMeep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
@@ -28,14 +32,11 @@ public class MeepMeepTesting {
                 .build();
 
         myBot.runAction(myBot.getDrive().actionBuilder(Positions.FAR.START)
-                .lineToY(Positions.modifyPose(Positions.FAR.SPIKEMARK_MID, 0, Constants.ROBOT_LENGTH / 2).y)
-                .lineToY(Positions.modifyPose(Positions.FAR.SPIKEMARK_MID, 0, Constants.ROBOT_LENGTH / 2).y + 5)
+                .lineToY(testingConstants.reflectY(Positions.modifyPose(Positions.FAR.SPIKEMARK_MID, 0, Constants.ROBOT_LENGTH / 2)).y)
+                .lineToY(testingConstants.reflectY(Positions.modifyPose(Positions.FAR.SPIKEMARK_MID, 0, Constants.ROBOT_LENGTH / 2)).y + 5)
                 .setTangent(Math.toRadians(180))
-                .splineToLinearHeading(Positions.vectorToPose(Positions.modifyPose(Positions.GENERAL.STACK_ONE, 1.3 * Constants.ROBOT_LENGTH, 0), 0), Math.toRadians(0))
-                .lineToX(Positions.modifyPose(Positions.GENERAL.STACK_ONE, Constants.ROBOT_LENGTH / 2, 0).x)
-                .lineToX(Positions.modifyPose(Positions.GENERAL.BACKDROP_MID, -Constants.TILE_LENGTH, 0).x)
-                .splineToConstantHeading(Positions.modifyPose(Positions.GENERAL.BACKDROP_MID, -Constants.ROBOT_LENGTH / 2, 0), Math.toRadians(0))
-
+                .splineToLinearHeading(testingConstants.reflectY(Positions.vectorToPose(Positions.modifyPose(Positions.GENERAL.STACK_ONE, 1.3 * Constants.ROBOT_LENGTH, testingConstants.Y_OFFSET), 0)), Math.toRadians(0))
+                .lineToX(testingConstants.reflectY(Positions.modifyPose(Positions.GENERAL.STACK_ONE, Constants.ROBOT_LENGTH / 2 + testingConstants.X_OFFSET, 0)).x)
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
