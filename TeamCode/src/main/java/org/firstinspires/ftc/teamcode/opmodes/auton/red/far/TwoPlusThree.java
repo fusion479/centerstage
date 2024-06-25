703cb2776e991909607390f025e6091095b5d233
Bi1ku <2008owenshi@gmail.com>
2024-06-25 15:18:36 -0400

    red 2+3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/red/far/TwoPlusThree.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/red/far/TwoPlusThree.java
@@ -2,35 +2,44 @@ package org.firstinspires.ftc.teamcode.opmodes.auton.red.far;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.CommandScheduler;
-import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.example.meepmeeptesting.Positions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-import org.firstinspires.ftc.teamcode.commands.arm.ArmScore;
-import org.firstinspires.ftc.teamcode.commands.deposit.DepositScore;
-import org.firstinspires.ftc.teamcode.subsystems.Arm;
-import org.firstinspires.ftc.teamcode.subsystems.Deposit;
-import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.CommandRobot;
+import org.firstinspires.ftc.teamcode.commands.auton.IntakeSetPower;
+import org.firstinspires.ftc.teamcode.commands.auton.IntakeUntilPixel;
+import org.firstinspires.ftc.teamcode.opmodes.auton.Trajectories;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
-import org.firstinspires.ftc.teamcode.subsystems.Lift;
-import org.firstinspires.ftc.teamcode.utils.CommandGroupAction;
+import org.firstinspires.ftc.teamcode.subsystems.camera.Camera;
+import org.firstinspires.ftc.teamcode.utils.CommandAction;
+import org.firstinspires.ftc.teamcode.utils.Wait;
 
 @Autonomous(name = "2+3 Red Far", group = "_Auto")
 public class TwoPlusThree extends CommandOpMode {
     private MultipleTelemetry multipleTelemetry;
-    private Drivetrain drive;
-    private Lift lift;
-    private Arm arm;
-    private Deposit deposit;
-    private Intake intake;
+    private CommandRobot robot;
+    private Camera camera;
 
     @Override
     public void initialize() {
         this.multipleTelemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-        this.arm = new Arm(hardwareMap, this.multipleTelemetry);
-        this.deposit = new Deposit(hardwareMap, this.multipleTelemetry);
+        this.robot = new CommandRobot(
+                super.hardwareMap,
+                new GamepadEx(this.gamepad1),
+                new GamepadEx(this.gamepad2),
+                this.multipleTelemetry,
+                Positions.FAR.START,
+                CommandRobot.Type.AUTON);
+        this.camera = new Camera(Camera.Color.RED, this.multipleTelemetry);
+        this.camera.initCamera(super.hardwareMap);
     }
 
     @Override
@@ -38,23 +47,79 @@ public class TwoPlusThree extends CommandOpMode {
         CommandScheduler.getInstance().enable();
         this.initialize();
 
-        super.waitForStart();
-
-        while (!super.isStopRequested() && super.opModeIsActive()) {
-            CommandScheduler.getInstance().run();
-
-            Actions.runBlocking(
-                    new CommandGroupAction(
-                            new SequentialCommandGroup(
-                                    new ArmScore(this.arm),
-                                    new DepositScore(this.deposit)
-                            )
-                    )
-            );
-
+        while (!super.isStarted()) {
+            this.multipleTelemetry.addData("Region:", this.camera.getRegion());
             this.multipleTelemetry.update();
         }
+        int region = this.camera.getRegion();
+        this.camera.stopStreaming();
 
+        Trajectories.Far FAR = new Trajectories(Camera.Color.RED, this.robot.getDrive()).new Far();
+        Action initialPath = region == 1 ? FAR.LEFT_SPIKEMARK : region == 2 ? FAR.MID_SPIKEMARK : FAR.RIGHT_SPIKEMARK;
+
+        Actions.runBlocking(new ParallelAction(
+                initialPath,
+                new SequentialAction(
+                        new CommandAction(new Wait(4000)),
+                        new CommandAction(new InstantCommand(() -> this.robot.getIntake().setPosition(Intake.ACCEPTING_POS))),
+                        new CommandAction(new Wait(500)),
+                        new CommandAction(this.robot.stack),
+                        new CommandAction(new IntakeUntilPixel(this.robot.getDeposit(), this.robot.getIntake())),
+                        new CommandAction(new Wait(250)),
+                        new CommandAction(new IntakeSetPower(this.robot.getIntake(), 500)),
+                        new CommandAction(new InstantCommand(() -> this.robot.getIntake().setPosition(Intake.ACCEPTING_POS))) // don't interfere
+                )
+        ));
+
+
+
+        Trajectories.General GENERAL = new Trajectories(Camera.Color.RED, this.robot.getDrive()).new General();
+        Action stackToBackdrop = region == 1 ? GENERAL.STACK_TO_LEFT_BACKDROP : region == 2 ? GENERAL.STACK_TO_MID_BACKDROP : GENERAL.STACK_TO_RIGHT_BACKDROP;
+
+        Actions.runBlocking(new ParallelAction(
+                stackToBackdrop,
+                new SequentialAction(
+                        new CommandAction(new Wait(6250)),
+                        new CommandAction(this.robot.scoreLow),
+                        new CommandAction(new Wait(2000)),
+                        new CommandAction(this.robot.scoreOne),
+                        new CommandAction(new Wait(250)),
+                        new CommandAction(this.robot.scoreTwo),
+                        new CommandAction(new Wait(1000))
+                )
+        ));
+
+        Action backdropToStack = region == 1 ? GENERAL.LEFT_BACKDROP_TO_STACK : region == 2 ? GENERAL.MID_BACKDROP_TO_STACK : GENERAL.RIGHT_BACKDROP_TO_STACK;
+
+        Actions.runBlocking(new ParallelAction(
+                backdropToStack,
+                new SequentialAction(
+                        new CommandAction(new Wait(5000)),
+                        new CommandAction(this.robot.stack),
+                        new CommandAction(new IntakeUntilPixel(this.robot.getDeposit(), this.robot.getIntake())),
+                        new CommandAction(new Wait(250)),
+                        new CommandAction(new IntakeSetPower(this.robot.getIntake(), 500)),
+                        new CommandAction(new InstantCommand(() -> this.robot.getIntake().setPosition(Intake.ACCEPTING_POS))) // don't interfere
+                )
+        ));
+
+        Actions.runBlocking(new ParallelAction(
+                stackToBackdrop,
+                new SequentialAction(
+                        new CommandAction(new Wait(6250)),
+                        new CommandAction(this.robot.scoreLow),
+                        new CommandAction(new Wait(2000)),
+                        new CommandAction(this.robot.scoreOne),
+                        new CommandAction(new Wait(250)),
+                        new CommandAction(this.robot.scoreTwo),
+                        new CommandAction(new Wait(1000))
+                )
+        ));
+
+        Actions.runBlocking(FAR.getPark());
+
+        CommandScheduler.getInstance().cancelAll();
         CommandScheduler.getInstance().disable();
+        CommandScheduler.getInstance().reset();
     }
 }
