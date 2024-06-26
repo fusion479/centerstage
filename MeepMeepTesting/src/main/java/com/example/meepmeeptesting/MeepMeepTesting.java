05ab4608843f55403fd3148cc709205eb88c9c8d
Bi1ku <2008owenshi@gmail.com>
2024-06-26 19:56:39 -0400

    2_3 progress

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -7,11 +7,10 @@ import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 
-
 class TestingConstants {
-    private final int color = 0; // 0 = blue, 1 = red
     public final double Y_OFFSET = -3.0;
     public final double X_OFFSET = 3.0;
+    private final int color = 0; // 0 = blue, 1 = red
 
     public Pose2d reflectY(Pose2d pose) {
         return color == 1 ? new Pose2d(pose.position.x, -pose.position.y, Math.toRadians(360) - pose.heading.toDouble()) : pose;
@@ -21,6 +20,7 @@ class TestingConstants {
         return color == 1 ? new Vector2d(vector.x, -vector.y) : vector;
     }
 }
+
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
@@ -32,13 +32,10 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(utils.reflectY(Positions.FAR.START))
-                .lineToY(utils.reflectY(Positions.modifyPose(Positions.FAR.SPIKEMARK_MID, 0, Constants.ROBOT_LENGTH / 2)).y)
-                .lineToY(utils.reflectY(Positions.modifyPose(Positions.FAR.SPIKEMARK_MID, 0, Constants.ROBOT_LENGTH / 2 + 5)).y)
+        myBot.runAction(myBot.getDrive().actionBuilder(utils.reflectY(Positions.vectorToPose(Positions.GENERAL.BACKDROP_MID, Math.toRadians(0))))
                 .setTangent(Math.toRadians(180))
-                .splineToLinearHeading(utils.reflectY(Positions.vectorToPose(Positions.modifyPose(Positions.GENERAL.STACK_ONE, 1.3 * Constants.ROBOT_LENGTH, utils.Y_OFFSET), 0)), Math.toRadians(0))
-                .lineToX(utils.reflectY(Positions.modifyPose(Positions.GENERAL.STACK_ONE, Constants.ROBOT_LENGTH / 2 + utils.X_OFFSET, 0)).x)
-                .build());
+                .splineToConstantHeading(utils.reflectY(new Vector2d(Positions.modifyPose(Positions.GENERAL.BACKDROP_MID, -Constants.TILE_LENGTH, 0).x, Positions.GENERAL.STACK_ONE.y)), Math.toRadians(180))
+                .lineToX(utils.reflectY(Positions.modifyPose(Positions.GENERAL.STACK_ONE, Constants.ROBOT_LENGTH / 2, 0)).x).build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
